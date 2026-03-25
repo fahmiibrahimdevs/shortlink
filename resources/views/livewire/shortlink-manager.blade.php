b29108aeb962266dd5a8642c5f78a14a9e5420e0
fahmiibrahimdevs <midragon.project@gmail.com>
2026-03-25 23:01:29 +0700

    feat: add button copy link

--- resources/views/livewire/shortlink-manager.blade.php
+++ resources/views/livewire/shortlink-manager.blade.php
@@ -109,7 +109,25 @@
                                     </span>
                                 </div>
                             </div>
-                            <div class="flex items-center pr-2">
+                            <div class="flex items-center gap-3 pr-2">
+                                <button
+                                    x-data="{ copied: false }"
+                                    @click="
+                                        navigator.clipboard.writeText('{{ url($link->short_code) }}');
+                                        copied = true;
+                                        setTimeout(() => copied = false, 2000);
+                                    "
+                                    class="text-cyan-500 hover:text-cyan-300 bg-cyan-950/30 hover:bg-cyan-900/50 border border-cyan-900/50 hover:border-cyan-500/50 p-3 rounded-xl transition-all duration-300 hover:shadow-[0_0_15px_rgba(6,182,212,0.3)] hover:scale-110"
+                                    title="COPY LINK"
+                                >
+                                    <svg x-show="!copied" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
+                                    </svg>
+                                    <svg x-show="copied" style="display: none;" class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                                    </svg>
+                                </button>
+                                
                                 <button 
                                     wire:click="deleteLink({{ $link->id }})"
                                     wire:confirm="PURGE RECORD? This action cannot be undone."
